ac53871d1f4b4c9a0d47c5d639db0a9a234d7db0
pawelros <rosiu@outlook.com>
2019-02-07 23:29:42 +0100

    install docker and add user to docker group

--- chef/cookbooks/raspberry/recipes/default.rb
+++ chef/cookbooks/raspberry/recipes/default.rb
@@ -1,9 +1,14 @@
 docker_installation_script 'default' do
     repo 'main'
-    script_url 'https://my.computers.biz/dist/scripts/docker.sh'
+    script_url 'https://gist.githubusercontent.com/pawelros/2ec289da8783ecb82e1fe951104dc3c3/raw/e76efc9e8dbde69bb802f0e8bcd85a8bdf0b9946/get-docker.sh'
     action :create
   end
 
-docker_image 'nginx-alpine' do
+  execute 'add rosiu to docker group' do
+    command 'sudo gpasswd -a rosiu docker && newgrp docker'
+  end
+
+docker_image 'nginx' do
+    tag '1.14.1-alpine'
     action :pull
   end
