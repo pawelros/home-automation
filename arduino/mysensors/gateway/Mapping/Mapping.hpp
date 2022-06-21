5d53aeb6ca8c7513001532703aa640ea3556a2b3
Pawel Rosinski <pawel.rosinski@blockfi.com>
2022-06-21 14:41:34 +0200

    fix pins

--- arduino/mysensors/gateway/Mapping/Mapping.hpp
+++ arduino/mysensors/gateway/Mapping/Mapping.hpp
@@ -51,12 +51,12 @@ std::vector<CustomSensor> customSensors = std::vector<CustomSensor>() = {
 
 // Pushbuttons declaration
 // Remember that names should be consistent with main loop in gateway.ino
-OneButton kuchnia(PIN_A26, true);
-OneButton jadalnia(PIN_A26, true);
-OneButton salon(PIN_A27, true);
-OneButton biuro(PIN_A28, true);
-OneButton przedpokoj(PIN_A29, true);
-OneButton balkon(PIN_A30, true);
-OneButton poddasze(PIN_A31, true);
-OneButton sypialnia(PIN_A32, true);
-OneButton stasiu(PIN_A33, true);
+OneButton kuchnia(26, true);
+OneButton jadalnia(26, true);
+OneButton salon(27, true);
+OneButton biuro(28, true);
+OneButton przedpokoj(29, true);
+OneButton balkon(30, true);
+OneButton poddasze(31, true);
+OneButton sypialnia(32, true);
+OneButton stasiu(33, true);
