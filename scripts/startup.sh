95c492f51413b7341bbd1aab9bc6b832505d7e0d
Pawel Rosinski <rosiu@outlook.com>
2022-07-03 14:30:35 +0200

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,7 +2,7 @@
 echo "waiting for HA to become responsive..."
 until $(curl --output /dev/null --silent --get --fail http://192.168.1.2:8123); do
     printf '.'
-    sleep 5
+    sleep 6
 done
 echo "running chromium."
 chromium --kiosk --enable-features=OverlayScrollbar --disk-cache-dir=/dev/null --disk-cache-size=1 http://192.168.1.2:8123/
