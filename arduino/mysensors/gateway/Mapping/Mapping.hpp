c8a1e526e2423d0512b4ea77d76b46c6f04b6cc1
Paweł Rosiński <dev.rosiv@gmail.com>
2022-09-22 21:58:44 +0200

    Update mapping

--- arduino/mysensors/gateway/Mapping/Mapping.hpp
+++ arduino/mysensors/gateway/Mapping/Mapping.hpp
@@ -34,9 +34,9 @@ const uint8_t STASIU_OSWIETLENIE_L1           = 11;
 const uint8_t STASIU_OSWIETLENIE_L2           = 12;
 const uint8_t LAZIENKA_DOL_OSWIETL            = 13;
 const uint8_t LAZIENKA_DOL_SW_LUSTRO          = 14;
-const uint8_t WENTYLATOR_LAZIENKA_WC          = 18;
-const uint8_t WENTYLATOR_LAZIENKA_PRYSZNIC    = 19;
-const uint8_t PIEC                            = 20;
+const uint8_t WENTYLATOR_LAZIENKA_WC          = 17;
+const uint8_t WENTYLATOR_LAZIENKA_PRYSZNIC    = 18;
+const uint8_t PIEC                            = 19;
 
 
 // Vector contaning child ID, description, output pin
@@ -55,9 +55,9 @@ std::vector<CustomSensor> customSensors = std::vector<CustomSensor>() = {
   { CustomSensor(STASIU_OSWIETLENIE_L2,           "Stasiu oswietlenie poziom 2",         29) },
   { CustomSensor(LAZIENKA_DOL_OSWIETL,            "Łazienka dół oświetlenie",            30) },
   { CustomSensor(LAZIENKA_DOL_SW_LUSTRO,          "Łazienka dół światło lustro",         31) },
-  { CustomSensor(WENTYLATOR_LAZIENKA_WC,          "Wentylator łazienka WC",              32) },
-  { CustomSensor(WENTYLATOR_LAZIENKA_PRYSZNIC,    "Wentylator łazienka Prysznic",        33) },
-  { CustomSensor(PIEC,                            "Piec gazowy ogrzewanie",              34) }
+  { CustomSensor(WENTYLATOR_LAZIENKA_WC,          "Wentylator łazienka WC",              34) },
+  { CustomSensor(WENTYLATOR_LAZIENKA_PRYSZNIC,    "Wentylator łazienka Prysznic",        35) },
+  { CustomSensor(PIEC,                            "Piec gazowy ogrzewanie",              36) }
 };
 
 // Pushbuttons declaration
